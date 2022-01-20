26dc71240a09fd11544be51fdf49e56b16c2f8b6
Seongha AHN <seongha7379@pusan.ac.kr>
2022-01-20 10:42:49 +0900

    Update bag_to_image.py

--- bag_to_image.py
+++ bag_to_image.py
@@ -35,7 +35,7 @@ try:
     pipeline = rs.pipeline()
     config = rs.config()
     rs.config.enable_device_from_file(config, args.input)
-    config.enable_stream(rs.stream.color,  640, 480, rs.format.rbg8, 30)
+    config.enable_stream(rs.stream.color, rs.format.rbg8, 30)
 
     # Start streaming from file
     pipeline.start(config)
@@ -45,10 +45,12 @@ try:
     while True:
         frames = pipeline.wait_for_frames()
         color_frame = frames.get_color_frame()
+        
         img = np.asanyarray(color_frame.get_data())
-        frame_idx = frame_idx + 1
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         
         cv2.imwrite('./image/'+str(frame_idx)+'.png', img)
-
+        
+        frame_idx = frame_idx + 1
 finally:
     pass
